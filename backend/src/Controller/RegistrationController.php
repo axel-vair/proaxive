28ebbcff13a4ce8f34cb58be1ce3366ef7e5823d
Axel Vair <avair@protonmail.com>
2025-01-15 09:29:13 +0100

    refactor token api register/login use API Plateform

---
+++ backend/src/Controller/RegistrationController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Attribute\MapQueryString;
+use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Attribute\Route;
+
+class RegistrationController extends AbstractController
+{
+    #[Route('/api/register', name: 'app_register')]
+    public function register(Request $request,
+                             EntityManagerInterface $entityManager,
+                             UserPasswordHasherInterface $passwordHasher): JsonResponse
+    {
+        $payload = $request->getPayload();
+        $user = new User();
+        $user->setEmail($payload->get('email'));
+        $user->setFirstName($payload->get('first_name'));
+        $user->setLastName($payload->get('last_name'));
+        $plaintextPassword = $payload->get('password');
+        $hashedPassword = $passwordHasher->hashPassword(
+            $user,
+            $plaintextPassword
+        );
+        $user->setPassword($hashedPassword);
+        $entityManager->persist($user);
+        $entityManager->flush();
+
+        return new JsonResponse(["success" => "User registred successfully"], 201);
+    }
+}
