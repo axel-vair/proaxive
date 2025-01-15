f086fb9674570eb426c0f5e4c475e8c256867d81
Axel Vair <avair@protonmail.com>
2025-01-15 13:32:05 +0100

    :bug: try to fix cors problem

--- backend/src/Controller/RegistrationController.php
+++ backend/src/Controller/RegistrationController.php
@@ -3,13 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpKernel\Attribute\MapQueryString;
-use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
